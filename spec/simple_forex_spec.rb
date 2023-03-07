8f5a8add6898a7ae62d0a6b3a5ef9892b77f3e25
Steve Condylios <steve.condylios@gmail.com>
2023-03-07 17:15:34 +1100

    Remove extraneous file, update GitHub Action workflow to run rspec tests, add a test

--- spec/simple_forex_spec.rb
+++ spec/simple_forex_spec.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe SimpleForex do
-  it "has a version number" do
+ 
+  # Default test
+ it "has a version number" do
     expect(SimpleForex::VERSION).not_to be nil
   end
 
+  it "all_currencies() provides at least 168 currencies" do
+    expect(all_currencies.length).to be >= 168 
+  end
+
+  # Default test
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
